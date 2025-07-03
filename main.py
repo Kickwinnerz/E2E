fa4a4e568ff697df0c1dd277016cfc912b3aba4d
Kick <kickwinner75@gmail.com>
2025-07-03 12:38:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -228,5 +228,4 @@ def stop_messages():
     return 'Messages sending stopped...'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
